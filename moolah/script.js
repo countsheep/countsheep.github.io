029ccd00eaff2ea0a1b1ab60caa90518c8a7fc51
countsheep <evamyeung@gmail.com>
2016-09-04 00:43:00 -0700

    oops adding

---
+++ moolah/script.js
@@ -0,0 +1,13 @@
+var Title = React.createClass({
+  render: function() {
+    return (
+      <div className="title ui center align container">
+        <h1>Hello, world! I am a CommentBox.</h1>
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <Title />,
+  document.getElementById('content')
+);
