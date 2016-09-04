3e66a91470ec3f5f79506b5fc2878cbce8583b10
countsheep <evamyeung@gmail.com>
2016-09-04 00:42:12 -0700

    test

--- moolah/scripts/script.js
+++ moolah/scripts/script.js
@@ -2,7 +2,7 @@ var Title = React.createClass({
   render: function() {
     return (
       <div className="title ui center align container">
-        <h1>Hello, world! I am a CommentBox.</h1>
+        <h1 class="ui header">Hello, world! I am a CommentBox.</h1>
       </div>
     );
   }
